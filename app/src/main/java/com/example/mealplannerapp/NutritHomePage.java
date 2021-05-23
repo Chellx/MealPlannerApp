7dbd58c685131ef532c73f212ac9c9e2f9819541
Michelle Bolger <michellebolgerlaois@gmail.com>
2021-05-23 12:01:19 +0100

    Created nutritionist profile and patient page

--- app/src/main/java/com/example/mealplannerapp/NutritHomePage.java
+++ app/src/main/java/com/example/mealplannerapp/NutritHomePage.java
@@ -2,7 +2,9 @@ package com.example.mealplannerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class NutritHomePage extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class NutritHomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nutrit_home_page);
+    }
+
+    public void goToNutritProfileScreen(View view) {
+
+
+        Intent intent = new Intent(this, NutritProfile.class);
+
+        startActivity(intent);
+    }
+
+    public void goToPatientScreen(View view) {
+
+
+        Intent intent = new Intent(this, NutritPatients.class);
+
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/mealplannerapp/NutritPatients.java
@@ -0,0 +1,14 @@
+package com.example.mealplannerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NutritPatients extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nutrit_patients);
+    }
+}
